1989fd71c05b40abdc8fbd56a1961dd74339401c
fhollo24 <143457544+fhollo24@users.noreply.github.com>
2023-09-05 11:41:28 -0400

    Add files via upload

---
+++ FH_README.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  5 11:10:56 2023
+
+@author: fhollo24
+"""
+
+#NGG6050 Fall 2023
+#Quant Neuro course
+#homework assignments and project codes for NGG6050
